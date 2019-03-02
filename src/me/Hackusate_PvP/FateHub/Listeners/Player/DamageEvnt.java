a05f1be4b0e03b8be038c1a7916004ce379e56de
Hackusate_PvP <supermftw@gmail.com>
2019-03-02 15:23:22 -0600

    Update 1.2

--- src/me/Hackusate_PvP/FateHub/Listeners/Player/DamageEvnt.java
+++ src/me/Hackusate_PvP/FateHub/Listeners/Player/DamageEvnt.java
@@ -1,6 +1,8 @@
 package me.Hackusate_PvP.FateHub.Listeners.Player;
 
 
+import me.Hackusate_PvP.FateHub.Main;
+import org.bukkit.Sound;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -23,6 +25,11 @@ public class DamageEvnt implements Listener {
     @EventHandler
     public void onD(EntityDamageEvent event) {
         if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (Main.getPlugin().getAPI().getSettingsManager().hasPop(player)) {
+                player.playSound(player.getLocation(), Sound.CHICKEN_EGG_POP, (float)1.0, (float)1.0);
+                event.setCancelled(true);
+            }
             event.setCancelled(true);
         }
     }
